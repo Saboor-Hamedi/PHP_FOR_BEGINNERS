b3c58db247754691dc7ff7a48cfce7461dfb6141
Saboor-Hamedi <janhamedi49@gmail.com>
2024-01-07 13:36:31 +0700

    Database clean up, adding http_build_query

--- App/Database/Database.php
+++ App/Database/Database.php
@@ -1,17 +1,38 @@
 <?php
 require_once __DIR__ . '/../functions/func.php';
-$host = 'db';
-$port = '3306';
-$db = 'blog';
-$username = 'admin';
-$pass = 'saboor123';
-// connect to PDO
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=UTF8";
-$pdo = new \PDO($dsn, $username, $pass);
-$statement  = $pdo->prepare('select * from posts');
-$statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($result as $x) {
-  echo '<li>' . $x['title'];
+class Database
+{
+  public $connection; // proterty
+  public function __construct()
+  {
+    $config = [
+      'host' => 'db',
+      'port' => '3306',
+      'dbname' => 'blog',
+      'username' => 'admin',
+      'pass' => 'saboor123'
+    ];
+
+
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
+    // connect to PDO
+    $this->connection = new \PDO($dsn, $config['username'], $config['pass'], [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+  }
+  public function query($query)
+  {
+
+    $statement  = $this->connection->prepare($query);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+}
+
+$db =  new Database();
+$posts = $db->query('select * from posts where id >1');
+
+foreach ($posts as $post) {
+  echo '<li>' . $post['title'] . '</li>';
 }
